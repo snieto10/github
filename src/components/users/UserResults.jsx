a7d5e366812b7049173fb59aaf782c0d322dcabd
Sergio Nieto <snieto10@gmail.com>
2022-11-24 09:39:00 -0500

    tg

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -9,23 +9,24 @@ function UserResults() {
   }, []);
 
   const fetchUsers = async () => {
-    const response = await fetch(`${process.env.REACT_APP_GITHUB_URL}/users`, {
-      headers: {
-        Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`,
-      },
-    });
+    const response = await fetch(`${process.env.REACT_APP_GITHUB_URL}/users`);
 
     const data = await response.json();
 
     setUsers(data);
     setLoading(false);
   };
-
-  return (
-    <div className="grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grids-cols-2">
-      UserResults
-    </div>
-  );
+  if (!loading) {
+    return (
+      <div className="grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grids-cols-2">
+        {users.map((user) => (
+          <h3>{user.login}</h3>
+        ))}
+      </div>
+    );
+  } else {
+    return <h3>Loading</h3>;
+  }
 }
 
 export default UserResults;
