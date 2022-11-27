473922c9a32a33c5cbb3d9bbfac543f6dca5928a
Sergio Nieto <snieto10@gmail.com>
2022-11-26 20:35:39 -0500

    56

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -1,31 +1,21 @@
-import { useEffect, useState } from "react";
+import { useContext } from "react";
+import Spinner from "../layout/spinner";
+import UserItem from "./UserItem";
+import GithubContext from "../../context/github/GithubContext";
 
 function UserResults() {
-  const [users, setUsers] = useState([]);
-  const [loading, setLoading] = useState(true);
+  const { users, loading } = useContext(GithubContext);
 
-  useEffect(() => {
-    fetchUsers();
-  }, []);
-
-  const fetchUsers = async () => {
-    const response = await fetch(`${process.env.REACT_APP_GITHUB_URL}/users`);
-
-    const data = await response.json();
-
-    setUsers(data);
-    setLoading(false);
-  };
   if (!loading) {
     return (
       <div className="grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grids-cols-2">
         {users.map((user) => (
-          <h3>{user.login}</h3>
+          <UserItem key={user.id} user={user} />
         ))}
       </div>
     );
   } else {
-    return <h3>Loading</h3>;
+    return <Spinner />;
   }
 }
 
