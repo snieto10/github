f6c929431c34be048b0689ee1ff5f4a6c85bae49
Sergio Nieto <snieto10@gmail.com>
2022-11-28 13:33:41 -0500

    search is working

--- src/components/users/UserSearch.jsx
+++ src/components/users/UserSearch.jsx
@@ -1,15 +1,29 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useContext } from "react";
+import GithubContext from "../../context/github/GithubContext";
 
 function UserSearch() {
   const [text, setText] = useState("");
 
+  const { users, searchUsers } = useContext(GithubContext);
+
   let handleChange = (e) => setText(e.target.value);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (text === "") {
+      alert("Please enter text");
+    } else {
+      searchUsers(text);
+      setText("");
+    }
+  };
 
   return (
     <div className="grid grid-cols-1 xl:grid-cols2 lg:grid-cols-2 md:grid-cols-2 mb-8 gap-8">
       <div>
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="form-control">
             <div className="relative">
               <input
@@ -19,16 +33,22 @@ function UserSearch() {
                 value={text}
                 onChange={handleChange}
               />
-              <button className="absolute top-0 right-0 rounded-l-none w-36 btn btn-lg">
+              <button
+                className="absolute top-0 right-0 rounded-l-none w-36 btn btn-lg"
+                onClick={handleSubmit}
+              >
                 Submit
               </button>
             </div>
           </div>
         </form>
       </div>
-      <div>
-        <button className="btn btn-ghost btn-lg">Clear</button>
-      </div>
+
+      {users.length > 0 && (
+        <div>
+          <button className="btn btn-ghost btn-lg">Clear</button>
+        </div>
+      )}
     </div>
   );
 }
